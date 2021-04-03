8cf9aa3a4b0a960bcba37722318b71553df08522
Pranam Hegde <pranamhegde3@gmail.com>
2021-04-03 17:15:38 +0530

    feat: added the installation documentation

---
+++ src/index.js
@@ -0,0 +1,3 @@
+var installationCodesnippet = document.getElementById('installation-codesnippet');
+
+installationCodesnippet.innerText = '<link rel="stylesheet" href="https://retinaui.netlify.app/public/base.css">';
