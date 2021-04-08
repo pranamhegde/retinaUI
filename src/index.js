c70024671ec835ce2025f517886acb7d76294475
Pranam Hegde <pranamhegde3@gmail.com>
2021-04-08 10:48:52 +0530

    feat: added the copy to clipboard feature to the code snippet

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 var installationCodesnippet = document.getElementById('installation-codesnippet');
 
+
 installationCodesnippet.innerText = '<link rel="stylesheet" href="https://retinaui.netlify.app/public/base.css">';
+
+const copyText = (HTMLElement) =>{
+    if(!HTMLElement){
+        return;
+    }
+
+    let elementText = HTMLElement.innerText;
+
+    let inputElement = document.createElement('input');
+    inputElement.setAttribute('value', elementText);
+    document.body.appendChild(inputElement);
+
+    inputElement.select();
+
+    document.execCommand('copy');
+    inputElement.parentNode.removeChild(inputElement);
+}
+
+const copyToCopied = (HTMLElement) =>{
+    var OriginalText = HTMLElement.innerText;
+    console.log(OriginalText)
+    HTMLElement.innerText = 'Copied!';
+    
+    setTimeout(() => {HTMLElement.innerHTML = 'Copy'}, 1500);
+
+}
+
+
+//For the Installation Element
+document.getElementById('copy-btn-installation-codesnippet').onclick = () => {
+    copyText(installationCodesnippet);
+    copyToCopied(document.getElementById('copy-btn-installation-codesnippet'));
+
+}
